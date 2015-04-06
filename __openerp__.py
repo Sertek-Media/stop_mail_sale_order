f5ffd9d36da1b73fb162e2dc8233e36a71d9afb7
Shivam Goyal <shivam1111@gmail.com>
2015-04-06 19:05:58 +0530

    First Commit

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Stop Sale Order Mail',
+    'category': 'sale',
+    'description': """
+    """,
+    'version': '1.0',
+    'depends':['base','sale'],
+    'data' : [
+    ],
+    'auto_install': False,
+}
